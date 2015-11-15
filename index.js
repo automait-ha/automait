a05eb20362899671c726098fad2923a27676e432
Adam Duncan <adam.duncan@clock.co.uk>
2015-11-15 17:23:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var path = require('path')
+  , bunyan = require('bunyan')
+  , logger = bunyan.createLogger({ name: 'automait' })
+  , config = null
+
+try {
+  config = require('../config.json')
+} catch (e) {
+  logger.error('A valid config.json must exist at: ' + path.resolve(__dirname + '/../config.json'))
+  process.exit(1)
+}
+
+var Automait = require('./lib/automait')
+  , automait = new Automait(logger, config)
+  , loadPlugins = require('./lib/plugin-loader')(automait, logger, config)
+
+loadPlugins(function (error, plugins) {
+  if (error) throw error
+  automait.setPlugins(plugins)
+  automait.init()
+})
